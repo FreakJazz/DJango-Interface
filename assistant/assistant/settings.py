85bc524bfb3d9d1125c5681c702a1766f80aa9e3
FreakJazz <jazzrb2307@hotmail.com>
2020-08-03 22:07:47 -0500

    Add superuser

--- assistant/assistant/settings.py
+++ assistant/assistant/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'rest_framework',
+    #'corsheaders'
     'post',
+
 ]
 
 MIDDLEWARE = [
@@ -109,15 +112,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
+
 
 
 # Static files (CSS, JavaScript, Images)
--- assistant/post/models.py
+++ assistant/post/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 class Users (models.Model):
-    Name = models.CharField(max_length=30)
-    Username = models.CharField(max_length=10)
-    Phone = models.IntegerField()
-    Email = models.EmailField()
-    Password = models.CharField(max_length=10)
+    id = models.AutoField(primary_key = True)
+    Name = models.CharField('Name', max_length=30, null = False, blank = False)
+    Username = models.CharField('Username',max_length=10, null = False, blank = False)
+    Phone = models.IntegerField('Phone',null = False, blank = False)
+    Email = models.EmailField('Email',null = False, blank = False)
+    Password = models.CharField(max_length=10, null = False, blank = False)
+    #date = models.DateField('Create Date', auto_now = False, auto_now_add = True)
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
 
     def __str__(self):
-        return self.Phone
+        return "{0},{1}".format(self.Username, self.Password)
         
